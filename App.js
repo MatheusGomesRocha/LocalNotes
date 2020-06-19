8595fdefa3c167550af44204325f10c14507e5d2
Matheus <matheusgomes192@hotmail.com>
2020-06-19 16:58:31 -0300

    Configurando React-Navigation

--- App.js
+++ App.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/lib/integration/react';
 import { store, persistor} from './src/store';
-import styled from 'styled-components';
+import { NavigationContainer } from '@react-navigation/native';
+import styled from 'styled-components/native';
+import MainStack from './src/navigators/MainStack';
 
 const Div = styled.View`
   flex: 1;
@@ -16,7 +18,9 @@ function App () {   /** PersistGate salva as informações em um AsyncStorage */
   return(  
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}> 
-          
+          <NavigationContainer>
+            <MainStack/>
+          </NavigationContainer>
       </PersistGate>
     </Provider>
       

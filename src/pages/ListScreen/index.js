d6b58975fdf9e9d210b713a321ad800d76e31bc7
Matheus <matheusgomes192@hotmail.com>
2020-06-20 16:12:07 -0300

    comentando o código da FlatList

--- src/pages/ListScreen/index.js
+++ src/pages/ListScreen/index.js
@@ -45,15 +45,24 @@ export default () => {
         });
     }, []);                                                         
 
-    function handleNotePress (index) {
+    function handleNotePress (index) {      /** Passa o index como parâmetro, para quando fazer o clique, saber diferenciar para qual anotação está indo */
         navigation.navigate('EditNote', {
-            key: index
+            key: index  /** Passa o index como uma key para diferenciar, é como se fosse um ID do BD no laravel */
         });
     }
     
-
+    /** 
+     * - Se o list for maior que 0, retorna o FlatList com os items da lista
+     * - <NotesList/> é um Flat list e recebe...
+     * - data={} a const que pega os valores com o hook useSelector do redux
+     * - renderItem={({item, index}) => <NoteItem />} 
+     * - o renderItem sempre recebe o item e o index como objetos. depois executa uma função chamando uma view que foi importada,
+     *   e passa props que podem ter qualquer nome, no caso tem que passar o item, e o index, e alguma função onPress se precisar
+     * - keyExtractor={(item, index) => index.toString()} retorna uma função que recebe item e index como parâmetro, e passa o index para string
+     * - Se o list for == 0, retorna um style personalizado.
+     */
     return (
-        <Container>
+        <Container>     
             {list.length > 0 &&
                 <NotesList 
                     data={list}
